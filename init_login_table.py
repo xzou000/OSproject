6883d686011ae7ad63a08061c56f799d3fef2d4c
Jin Hui Chen <Jin Hui Chen>
2016-12-10 19:36:48 -0500

    add a file

---
+++ init_login_table.py
@@ -0,0 +1,11 @@
+__author__ = 'hanguup'
+import sqlite3
+
+def account_table():
+    connect = sqlite3.connect('login.db')
+    connect.execute("CREATE TABLE USERS(USERNAME TEXT NOT NULL,PASSWORD NOT NULL, MONEY REAL, ACTIVATE INTEGER, FLAG INTEGER)")
+    connect.execute("INSERT INTO USERS VALUES(?, ?,?,?,?)", ('sample1', '123',0,1,0))
+    connect.commit()
+
+    connect.close()
+account_table()
